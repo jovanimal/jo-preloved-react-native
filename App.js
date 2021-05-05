3311aa79db7eb311007d3ed307537bf907a0baca
jovanimal <kylcg@hotmail.com>
2021-05-05 21:28:30 +0800

    Add text component for universal styling

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import AppText from './app/components/AppText';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          height: 100,
-          shadowOffset: { width: 10, height: 10 },
-          shadowOpacity: 1,
-          shadowRadius: 10,
-        }}
-      ></View>
+    <View 
+      style={{ 
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
