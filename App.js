24375fdb78ba84a40dc25d55e9594da441a0421a
jovanimal <kylcg@hotmail.com>
2021-05-09 22:20:34 +0800

    Add dropdown menu for category

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Text,
+  Switch,
+} from 'react-native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingScreen from './app/screens/ListingScreen';
@@ -10,9 +17,29 @@ import AccountScreen from './app/screens/AccountScreen';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
+import AppText from './app/components/AppText';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
-    <ListingScreen />
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+  FlatList,
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppText from './AppText';
+import Screen from '../components/Screen';
+import PickerItem from '../components/PickerItem';
+
+import defaultStyles from '../config/styles';
+
+const AppPicker = props => {
+  const {
+    icon,
+    items,
+    selectedItem,
+    onSelectItem,
+    placeholder,
+    ...otherProps
+  } = props;
+
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setShowModal(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal visible={showModal} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setShowModal(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={item => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => {
+                  setShowModal(false);
+                  onSelectItem(item);
+                }}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    flexDirection: 'row',
+    width: '100%',
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+export default AppPicker;
