3b53126eb34a8184c0f8918cb63608998b021f5d
jovanimal <kylcg@hotmail.com>
2021-05-17 21:33:20 +0800

    Allow access to the media library

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Button,
-  TextInput,
-  Text,
-  Switch,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+import { Button, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <ListingEditScreen />
-  );
+  const [imageUri, setImageUri] = useState();
+
+
+  const requestPermission = async () => {
+
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted')
+      alert('You need to enable permission to access to Library');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri)
+      
+    } catch (error) {
+      console.log('Error reading an image', error)
+    }
+  }
+
+  return <Screen>
+    <Button title="Select Image" onPress={selectImage} />
+    <Image source={{ uri : imageUri }} style={{ width: 200, height: 200 }} />
+  </Screen>;
 }
