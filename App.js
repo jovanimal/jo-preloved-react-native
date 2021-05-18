e19df8ae596abd528723d2d13b602be9275880df
jovanimal <kylcg@hotmail.com>
2021-05-18 22:32:52 +0800

    Add ImageInput and ImageInputList to allow user to upload image

--- App.js
+++ App.js
@@ -1,37 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import React, { useState } from 'react';
 
 import Screen from './app/components/Screen';
-import { Button, Image } from 'react-native';
+import ImageInputList from './app/components/ImageInputList';
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-
-  const requestPermission = async () => {
-
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted')
-      alert('You need to enable permission to access to Library');
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri]);
   };
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
+  };
 
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri)
-      
-    } catch (error) {
-      console.log('Error reading an image', error)
-    }
-  }
-
-  return <Screen>
-    <Button title="Select Image" onPress={selectImage} />
-    <Image source={{ uri : imageUri }} style={{ width: 200, height: 200 }} />
-  </Screen>;
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+        onChangeImage={uri => setImageUri(uri)}
+      />
+    </Screen>
+  );
 }
