6febecbe10f0d581df271f2a13522f7a10639c7d
jovanimal <kylcg@hotmail.com>
2021-05-23 23:03:55 +0800

    Install react-navigation and insert navigation between screens

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import React, { useState } from 'react';
+import { Button, Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 import Screen from './app/components/Screen';
-import ImageInputList from './app/components/ImageInputList';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+
+// If the component is not used as a Screen,
+// we can use useNagivation hooks to access the navigation props
+// const Link = () => {
+//   const navigation = useNavigation();
+
+//   return (
+//     <Button title="Click" onPress={() => navigation.navigate('TweetDetails')} />
+//   )
+// }
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.push('TweetDetails', { id: 1 })}
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id}</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    // https://reactnavigation.org/docs/stack-navigator#options
+    screenOptions={{
+      headerStyle: { backgroundColor: 'dodgerblue' },
+        headerTintColor: 'white',
+    }}
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      // can override screenOption by using options props in individual Screen
+      options={{
+        headerStyle: { backgroundColor: 'tomato' },
+        headerTintColor: 'white',
+        headShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
