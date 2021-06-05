a1ee9a15b3741948870c88588946c0fa3f515c60
jovanimal <kylcg@hotmail.com>
2021-06-05 16:35:40 +0800

    Implement AsyncStorage for offline caching

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {
-  Image,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Image } from 'react-native-expo-image-cache';
 
 import AppText from './AppText';
 import colors from '../config/colors';
 
 const Card = props => {
-  const { title, subTitle, imageUrl, onPress } = props;
+  const { title, subTitle, imageUrl, onPress, thumbnailUrl } = props;
 
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image
+          style={styles.image}
+          tint="light"
+          preview={{ uri: thumbnailUrl }}
+          uri={imageUrl}
+        />
         <View style={styles.detailsContainer}>
           <AppText style={styles.title}>{title}</AppText>
           <AppText style={styles.subTitle}>{subTitle}</AppText>
